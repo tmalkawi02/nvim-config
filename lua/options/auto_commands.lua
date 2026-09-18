bfebf70c9062e3646da372a64f38b9e51b8e06dd
tmalkawi02 <malkawitalal@gmail.com>
2026-09-18 18:30:03 +0200

    chore: add sql and ts to format

--- lua/options/auto_commands.lua
+++ lua/options/auto_commands.lua
@@ -4,7 +4,7 @@ if not status then
 end
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = { "*.lua", "*.cpp" },
+	pattern = { "*.lua", "*.cpp", "*.ts", "*.sql" },
 	callback = function(args)
 		conform.format({ bufnr = args.buf })
 	end,
